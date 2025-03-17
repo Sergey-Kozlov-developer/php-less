e3da0816007fb2ea376ef07f793e21df25e899f9
Sergey-Kozlov-developer <vmf.serge.kozlov@gmail.com>
2025-03-17 20:11:12 +0300

    varibles

---
+++ www/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>LAMP STACK</title>
+        <link rel="shortcut icon" href="/assets/images/favicon.svg" type="image/svg+xml">
+        <link rel="stylesheet" href="/assets/css/bulma.min.css">
+    </head>
+    <body>
+        <?php
+
+        $a = 10;
+        $b = 5;
+
+        if ($a > $b) {
+            echo "$a больше $b";
+        } else {
+            echo "$a меньше $b";
+        }
+
+
+        ?>
+    </body>
+</html>
