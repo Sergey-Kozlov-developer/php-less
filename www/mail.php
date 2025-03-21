53b57ba37be2994db14267ddfbe3356c647b87da
Sergey-Kozlov-developer <vmf.serge.kozlov@gmail.com>
2025-03-21 15:59:09 +0300

    ajax

--- www/mail.php
+++ www/mail.php
@@ -1,60 +1,62 @@
 <?php
 
+if (
+	!empty(trim($_POST['name']))
+	&& !empty(trim($_POST['email']))
+	&& filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)
+	&& !empty(trim($_POST['message']))
+) {
+	$mail_to = "info@mail.com"; // Email куда отправлено письмо
+	$email_from = "info@gmail.com"; // указываем от кого отправлено письмо email
+	$name_from = "Личный сайт портфолио"; // указываем от кого отправлено письмо, имя
+	$subject = "Сообщение с сайта"; // тема письма
 
-// echo "Hello";
-// 2. распечатаем полученные данные из формы
-// echo "<pre>";
-// print_r($_POST);
-// echo "</pre>";
+	//формируем текст письма
+	$message = "Вам пришло новое сообщение с сайта: <br><br>\n" .
+		"<strong>Имя отправителя: </strong>" . strip_tags(trim($_POST["name"])) . "<br>\n" .
+		"<strong>Email отправителя: </strong>" . strip_tags(trim($_POST["email"])) . "<br>\n" .
+		"<strong>Сообщение: </strong>" . strip_tags(trim($_POST["message"]));
+	// формируем тему письма
+	$subject = "=?utf-8?B?" . base64_encode("Сообщение с сайта!") . "?=";
+	// формируем доп заголовки письма
+	$headers = "MIME-Version: 1.0" . PHP_EOL .
+		"Content-Type: text/html; charset=utf-8" . PHP_EOL .
+		"From: " . "=?utf-8?B?" . base64_encode($name_from) . "?=" . "<" . $email_from . ">" . PHP_EOL .
+		"Reply-To: " . $email_from . PHP_EOL;
 
-// 3 распечатаем отдельно элементы из $_POST
-// echo $_POST['name'];
-// echo "<br>";
-// echo $_POST['email'];
-// echo "<br>";
-// echo $_POST['message'];
-// echo "<br>";
+	$mailResult = mail($mail_to, $subject, $message, $headers);
 
-/** ===============ВАРИАНТ 1 ================ */
-// mail('info@mail.com', "Сообщение с сайта", $_POST["message"]);
+	// сообщаем на frontend об отправке. закодируем в JSON
 
-
-/** ===============ВАРИАНТ 2 ================ */
-
-// trim() обрезает лишние пробелы
-// формируем текст письма
-/*
-$message = "Вам пришло новое сообщение с сайта: <br><br>\n" .
-	"<strong>Имя отправителя: </strong>" . strip_tags(trim($_POST["name"])) . "<br>\n" .
-	"<strong>Email отправителя: </strong>" . strip_tags(trim($_POST["email"])) . "<br>\n" .
-	"<strong>Сообщение: </strong>" . strip_tags(trim($_POST["message"]));
-
-//отправляем письмо
-mail("info@mail.com", "Сообщение с сайта", $message);
- */
-/** ===============ВАРИАНТ 3 ================ */
-$mail_to = "info@mail.com";
-$email_from = "info@gmail.com";
-$name_from = "Личный сайт портфолио";
-$subject = "Сообщение с сайта";
-//формируем текст письма
-$message = "Вам пришло новое сообщение с сайта: <br><br>\n" .
-	"<strong>Имя отправителя: </strong>" . strip_tags(trim($_POST["name"])) . "<br>\n" .
-	"<strong>Email отправителя: </strong>" . strip_tags(trim($_POST["email"])) . "<br>\n" .
-	"<strong>Сообщение: </strong>" . strip_tags(trim($_POST["message"]));
-// формируем тему письма
-$subject = "=?utf-8?B?" . base64_encode("Сообщение с сайта!") . "?=";
-// формируем доп заголовки письма
-$headers = "MIME-Version: 1.0" . PHP_EOL .
-	"Content-Type: text/html; charset=utf-8" . PHP_EOL .
-	"From: " . "=?utf-8?B?" . base64_encode($name_from) . "?=" . "<" . $email_from . ">" . PHP_EOL .
-	"Reply-To: " . $email_from . PHP_EOL;
-
-$mailResult = mail($mail_to, $subject, $message, $headers);
-
-if ($mailResult) {
-	header("Location: thankyou.html");
+	if ($mailResult) {
+		$response = [
+			"status" => true,
+			"message" => "Отправлено успешно"
+		];
+	} else {
+		$response = [
+			"status" => false,
+			"message" => "Произошла ошибка"
+		];
+	}
+	echo json_encode($response);
 } else {
+	$response = [
+		"status" => false,
+		"message" => []
+	];
+	if (empty(trim($_POST["name"]))) {
+		$response["message"][] = "Поле 'имя' не может быть пустым";
+	}
+	if (empty(trim($_POST["email"]))) {
+		$response["message"][] = "Поле 'email' не может быть пустым";
+	}
+	if (!filter_var((trim($_POST["email"], FILTER_VALIDATE_EMAIL)))) {
+		$response["message"][] = "Введите верный формат email";
+	}
+	if (empty(trim($_POST["message"]))) {
+		$response["message"][] = "Поле 'сообщение' не может быть пустым";
+	}
 
-	header("Location: error.html");
+	echo json_encode($response);
 }
