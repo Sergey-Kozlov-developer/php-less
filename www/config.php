fdc9c3ca8bc4d3fde60a353adf069ebf3a166f19
Sergey-Kozlov-developer <vmf.serge.kozlov@gmail.com>
2025-03-26 11:03:25 +0300

    filmoteka mvcup db

--- www/config.php
+++ www/config.php
@@ -1,6 +1,8 @@
 <?php
 
-define('MYSQL_SERVER', 'database');
-define('MYSQL_USER', 'root');
-define('MYSQL_PASSWORD', 'tiger');
-define('MYSQL_DB', 'filmoteka');
+// define('MYSQL_SERVER', 'https://dbs.timeweb.com/pma/index.php');
+define('MYSQL_SERVER', "83.147.245.109");
+// define('MYSQL_PORT', 3306);
+define('MYSQL_USER', 'gen_user');
+define('MYSQL_PASSWORD', '0Z\b/7yxn/Ld67');
+define('MYSQL_DB', 'default_db');
