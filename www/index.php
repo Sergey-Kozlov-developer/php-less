33670e5d879a0634dba971d4061a8d3835f223d0
Sergey-Kozlov-developer <vmf.serge.kozlov@gmail.com>
2025-04-11 09:42:38 +0300

    redbean

--- www/index.php
+++ www/index.php
@@ -1,184 +1,44 @@
 <?php
+require "db.php";
 
+// создаем нове записи bd 
+$course = R::dispense('courses');
+$course->title = "Курс по верстке";
+$course->tuts = 10;
+$course->homework = 8;
+$course->level = "Для новичков";
+$course->title = "Курс по React";
+$course->tuts = 101;
+$course->homework = 8;
+$course->level = "Для новичков";
+$course->title = "Курс по Laravel";
+$course->tuts = 1034;
+$course->homework = 23;
+$course->level = "Для midle";
+R::store($course);
 
+// получение всех записей
+$courses = R::find('courses');
 
-// соединение с базой данных
-$db = new PDO('mysql:host=database;dbname=films', 'root', 'tiger');
-$sql = "SELECT * FROM films";
-$result = $db->query($sql);
-
-$result->bindColumn('id', $id);
-$result->bindColumn('title', $title);
-$result->bindColumn('genre', $genre);
-$result->bindColumn('year', $year);
-echo "<h2>Вывод записей из резудьтата по одной: </h2>";
-
-while ($result->fetch(PDO::FETCH_ASSOC)) {
-	echo "ID: {$id} <br>";
-	echo "Название: {$title} <br>";
-	echo "Жанр: {$genre} <br>";
-	echo "Год: {$year} <br>";
+foreach ($courses as $course) {
+	echo "ID: " . $course->id . "<br>";
+	echo "Название : " . $course->title . "<br>";
+	echo "Кол-во уроков : " . $course->tuts . "<br>";
+	echo "Уровень: " . $course->level . "<br>";
+	echo "<hr>";
 }
 
+/// обновление в БД 
+$course = R::load('courses', 2);
+// print_r($course);
+echo "ID: " . $course->id . "<br>";
+echo "Название : " . $course->title . "<br>";
+echo "Кол-во уроков : " . $course->tuts . "<br>";
+echo "Уровень: " . $course->level . "<br>";
+echo "<hr>";
+$course->title = "Laravel - уровень 3";
+R::store($course);
 
-###################################
-// варианты sql запрос формируется на основании данных,
-// получаемых от пользователя.
-#####################################
-// Получение данных без защиты от SQL инъекций
-// Так не стоит делать, если данные приходят от пользователя 
-$db = new PDO('mysql:host=database;dbname=profile', 'root', 'tiger');
-$username = 'Yura';
-$password = '123456';
-
-$username = htmlentities($username);
-$password = htmlentities($password);
-
-$sql = "SELECT * FROM users WHERE name = '{$username}' AND password = '{$password}' LIMIT 1";
-
-$result = $db->query($sql);
-
-if ($result->rowCount() == 1) {
-	$user = $result->fetch(PDO::FETCH_ASSOC);
-	echo "Имя пользователя: {$user['name']} <br>";
-	echo "Пароль пользователя: {$user['password']}";
-}
-###############################################
-// С защитой от SQL инъекций - (в Ручном режиме)
-// Так делать нежелательно, но допустимо
-
-$db = new PDO('mysql:host=database;dbname=profile', 'root', 'tiger');
-// Допустим мы получили из массива $POST указанные ниже переменные
-$username = 'Yura';
-$password = '123456';
-
-$username = htmlentities($username);
-$password = htmlentities($password);
-
-// Избавляем от лишних кавычек
-// Экранируем все символы
-$username = $db->quote($username);
-$username = strtr($username, array('_' => '\_', '%' => '\%'));
-$password = $db->quote($password);
-$password = strtr($password, array('_' => '\_', '%' => '\%'));
-
-$sql = "SELECT * FROM users WHERE name = '{$username}' AND password = '{$password}' LIMIT 1";
-
-$result = $db->query($sql);
-
-if ($result->rowCount() == 1) {
-	$user = $result->fetch(PDO::FETCH_ASSOC);
-	echo "Имя пользователя: {$user['name']} <br>";
-	echo "Пароль пользователя: {$user['password']}";
-}
-#######################
-
-// С защитой от SQL инъекций - (в Автоматическом режиме)
-
-// 1-й вариант
-
-$sql = "SELECT * FROM users WHERE name = :username AND password = :password LIMIT 1";
-
-$stmt = $db->prepare($sql);
-
-$username = 'Yura';
-$password = '123456';
-
-$username = htmlentities($username);
-$password = htmlentities($password);
-
-$stmt->bindValue(':username', $username);
-$stmt->bindValue(':password', $password);
-$stmt->execute();
-
-//Если не хотим для каждого значения вызывать метод bindValue,
-// то можно сразу писать в ->execute
-// $stmt->execute(array(':username' => $username, ':password' => $password));
-$stmt->bindColumn('name', $name);
-$stmt->bindColumn('email', $email);
-
-echo "<h2>Выборка записи с автоматической защитой от инъекции</h2>";
-$stmt->fetch();
-echo "Имя пользователя: {$name} <br>";
-echo "Email пользователя: {$email} <br>";
-
-// 2-1 вариант
-
-$sql = "SELECT * FROM users WHERE name = ? AND password = ? LIMIT 1";
-
-$stmt = $db->prepare($sql);
-
-$username = 'Yura';
-$password = '123456';
-
-$username = htmlentities($username);
-$password = htmlentities($password);
-
-$stmt->bindValue(1, $username);
-$stmt->bindValue(2, $password);
-$stmt->execute();
-//Или можно короче сделать
-// $stmt->execute(array($username, $password));
-
-$stmt->bindColumn('name', $name);
-$stmt->bindColumn('email', $email);
-
-echo "<h2>Выборка записи с автоматической защитой от инъекции</h2>";
-$stmt->fetch();
-echo "Имя пользователя: {$name} <br>";
-echo "Email пользователя: {$email} <br>";
-
-########################
-// ВСТАВКА ДАННЫХ В БД
-
-$db = new PDO('mysql:host=database;dbname=profile', 'root', 'tiger');
-
-$sql = "INSERT INTO users (name, email) VALUES (:name, :email)";
-$stmt = $db->prepare($sql);
-
-$username = "Yura";
-$useremail = "reno@mail.com";
-
-$stmt->bindValue(':name', $username);
-$stmt->bindValue(':email', $useremail);
-$stmt->execute();
-
-
-echo "<p>Было затронуто строк: " . $stmt->rowCount() . "</p>";
-echo "<p>ID вставленной записи: " . $db->lastInsertId() . "</p>";
-
-######################
-// Обновление данных
-$db = new PDO('mysql:host=database;dbname=profile', 'root', 'tiger');
-
-$sql = "UPDATE users SET name = :name WHERE id = :id";
-
-$stmt = $db->prepare($sql);
-
-$username = "New Flash";
-$id = 2;
-
-$stmt->bindValue(':name', $username);
-$stmt->bindValue(':id', $id);
-$stmt->execute();
-
-echo "<p>Было затронуто строк: " . $stmt->rowCount() . "</p>";
-echo "<p>ID вставленной записи: " . $db->lastInsertId() . "</p>";
-//Если надо обновить несколько строк, то делается так:
-$sql = "UPDATE users SET name = :name, email = :email WHERE id = :id";
-
-
-####################
-// Удаление данных
-
-$db = new PDO('mysql:host=database;dbname=profile', 'root', 'tiger');
-
-$sql = "DELETE FROM users WHERE name = :name";
-$stmt = $db->prepare($sql);
-
-$username = "New Flash";
-
-$stmt->bindValue(':name', $username);
-$stmt->execute();
-
-echo "<p>Было затронуто строк: " . $stmt->rowCount() . "</p>";
+// удаление из БД 
+$course = R::load('courses', 2);
+R::trash($course);
